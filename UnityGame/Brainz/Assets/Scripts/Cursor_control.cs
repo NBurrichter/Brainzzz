10d82aa520d80238d3cd78f8beb5f19e76685408
Andreas <Andi.zenglein@t-online.de>
2015-10-23 12:11:16 +0200

    Added lcoked Cursor_mode
    Cursor can now be locked in the center of the view

---
+++ UnityGame/Brainz/Assets/Scripts/Cursor_control.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cursor_control : MonoBehaviour {
+
+
+    private bool bIsCursorLocked=false;
+    private CursorLockMode wantedMode;
+
+    // Use this for initialization
+    void Start () {
+        wantedMode = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	        if(Input.GetButtonDown("Fire3"))
+        {
+            Debug.Log("´change Cursor mode");
+            if (bIsCursorLocked == false)
+            {
+                Debug.Log("Cursor now locked");
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.Locked;
+                bIsCursorLocked = true;
+            }
+            else
+            {
+                wantedMode = CursorLockMode.None;
+                Debug.Log("Cursor now visible");
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+                bIsCursorLocked = false;
+            }
+        }
+	}
+}
