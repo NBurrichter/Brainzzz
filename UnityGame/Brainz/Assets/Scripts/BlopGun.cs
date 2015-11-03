06838beda9baa24fec6bb805155e47dc220d8eda
Andreas <Andi.zenglein@t-online.de>
2015-11-03 12:56:18 +0100

    Fixed Aiming and shooting
    +the Player now shoots more accurate

--- UnityGame/Brainz/Assets/Scripts/BlopGun.cs
+++ UnityGame/Brainz/Assets/Scripts/BlopGun.cs
@@ -22,14 +22,14 @@ public class BlopGun : MonoBehaviour {
 		if(Input.GetButtonDown("Fire1"))
 		{
 
-            Instantiate(Blop1Prefab, vPlayerPos + AimingControl.aimingControlSingleton.GetSpawnPosition(),Quaternion.identity);
+            Instantiate(Blop1Prefab, AimingControl.aimingControlSingleton.gameObject.transform.position + AimingControl.aimingControlSingleton.GetSpawnPosition(),Quaternion.identity);
 
 		}
 
 		if(Input.GetButtonDown("Fire2"))
 		   {
 
-			Instantiate(Blop2Prefab, vPlayerPos + AimingControl.aimingControlSingleton.GetSpawnPosition(), Quaternion.identity);
+			Instantiate(Blop2Prefab, AimingControl.aimingControlSingleton.gameObject.transform.position + AimingControl.aimingControlSingleton.GetSpawnPosition(), Quaternion.identity);
 		}
 	}
 }
