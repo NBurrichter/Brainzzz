613a39c222e0f39b126f7d56f017e08611c05843
Niklas Burrichter <niklas.burrichter@stud.h-da.de>
2015-11-04 10:55:22 +0100

    Second character Prototype. Gets continued in a new Branch.

---
+++ UnityGame/Brainz/Assets/Scripts/NavmeshTestNavigation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavmeshTestNavigation : MonoBehaviour {
+
+    private NavMeshAgent agent;
+
+    public GameObject target;
+
+	void Start ()
+    {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	void Update ()
+    {
+        agent.destination = target.transform.position;
+	}
+}
