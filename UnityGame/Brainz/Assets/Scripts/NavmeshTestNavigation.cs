a3314c298f64cb17a2eebf15529ce944978bd5ef
Niklas Burrichter <niklas.burrichter@stud.h-da.de>
2015-11-04 14:31:17 +0100

    Extended NPC behaviour

--- UnityGame/Brainz/Assets/Scripts/NavmeshTestNavigation.cs
+++ UnityGame/Brainz/Assets/Scripts/NavmeshTestNavigation.cs
@@ -5,20 +5,27 @@ public class NavmeshTestNavigation : MonoBehaviour {
 
     private NavMeshAgent agent;
     private bool active;
+    private int nextWaypointNumber;
+    private Rigidbody rb;
 
-    public GameObject target;
+    public GameObject[] waypoints;
+    
+    //public GameObject target;
 
 	void Start ()
     {
         agent = GetComponent<NavMeshAgent>();
         active = true;
+        nextWaypointNumber = 0;
+        rb = GetComponent<Rigidbody>();
 	}
 
     void Update()
     {
         if (active)
         {
-            agent.destination = target.transform.position;
+            //agent.destination = target.transform.position;
+            agent.SetDestination(waypoints[nextWaypointNumber].transform.position);
         }
     }
 
@@ -28,11 +35,14 @@ public class NavmeshTestNavigation : MonoBehaviour {
         {
             agent.enabled = true;
             active = true;
+            rb.isKinematic = true;
+
         }
         else
         {
             agent.enabled = false;
             active = false;
+            rb.isKinematic = false;
         }
     }
 }
