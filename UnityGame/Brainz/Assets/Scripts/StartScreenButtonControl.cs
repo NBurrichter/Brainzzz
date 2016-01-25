c9d7bda58a257d56e3f3283002b4c1285e1c55b6
Andreas <Andi.zenglein@t-online.de>
2016-01-25 14:29:21 +0100

    Added StartScreen

---
+++ UnityGame/Brainz/Assets/Scripts/StartScreenButtonControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreenButtonControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    public void StartButtonpressed()
+    {
+       Application.LoadLevelAsync(2);
+    } 
+
+    public void QuitButtonPressed()
+    {
+        Application.Quit();
+    }
+}
