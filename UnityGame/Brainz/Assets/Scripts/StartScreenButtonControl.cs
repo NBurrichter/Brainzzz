784bee750cd691ac35c695c4e0719dab1490ce17
Andreas <Andi.zenglein@t-online.de>
2016-01-27 10:45:42 +0100

    Added Background-video
    +Added Background-video
    +Improved starting options

--- UnityGame/Brainz/Assets/Scripts/StartScreenButtonControl.cs
+++ UnityGame/Brainz/Assets/Scripts/StartScreenButtonControl.cs
@@ -1,26 +1,45 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+
 
 public class StartScreenButtonControl : MonoBehaviour {
 
+    private bool bStartGameEnabled = true;
+
 	// Use this for initialization
 	void Start () {
-	
+       
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+
+
+
+        if (Input.anyKeyDown && bStartGameEnabled == true)
+            Application.LoadLevel(2);
 	}
 
 
     public void StartButtonpressed()
     {
-       Application.LoadLevelAsync(2);
+       Application.LoadLevel(2);
     } 
 
     public void QuitButtonPressed()
     {
         Application.Quit();
+    }
+
+    public void MouseEnterQuitButton()
+    {
+        bStartGameEnabled = false;
+    }
+
+    public void MouseLeaveQuitButton()
+    {
+        bStartGameEnabled = true;
     }
 }
