abeccc1547d6550b8cfaf0751f4b34b10fb39b46
Niklas Burrichter <niklas.burrichter@stud.h-da.de>
2016-01-28 17:04:04 +0100

    Finished Intro Room

---
+++ UnityGame/Brainz/Assets/Scripts/LoadLevelWhenPlayerEntersAndSonFinished.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevelWhenPlayerEntersAndSonFinished : MonoBehaviour {
+
+    public string levelName;
+    public GameObject son;
+
+    public void OnTriggerStay(Collider col)
+    {
+        Debug.Log(col.gameObject.name);
+        if (col.gameObject.name == "Player")
+        {
+            if (son.GetComponent<FindTestPath>().finished)
+            {
+                Application.LoadLevel(levelName);
+            }
+        }
+    }
+}
