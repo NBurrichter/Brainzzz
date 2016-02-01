fbe678fd5749da1e701124f7e0aa6dbb7431559d
Andreas <Andi.zenglein@t-online.de>
2016-02-01 14:22:27 +0100

    Added new Background-Sound

--- UnityGame/Brainz/Assets/Scripts/StartScreenButtonControl.cs
+++ UnityGame/Brainz/Assets/Scripts/StartScreenButtonControl.cs
@@ -23,14 +23,14 @@ public class StartScreenButtonControl : MonoBehaviour {
         if (Input.anyKeyDown && bStartGameEnabled == true)
         {
             Cursor.SetCursor(t2DCrosshair, vTextureOffset, CursorMode.Auto);
-            Application.LoadLevel(2);
+            Application.LoadLevel(1);
         }
 	}
 
 
     public void StartButtonpressed()
     {
-       Application.LoadLevel(2);
+       Application.LoadLevel(1);
     } 
 
     public void QuitButtonPressed()
